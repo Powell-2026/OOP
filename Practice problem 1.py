70a724a61295b83bdb531e85a2e6af42494ee2e9
Powell-2026 <elijah.powell2026@gmail.com>
2026-08-28 08:58:08 -0500

    Add files via upload

---
+++ Practice problem 1.py
@@ -0,0 +1,14 @@
+print("Enter your name:")
+Name = input()
+
+print("Enter basic pay:")
+Pay=int(input())
+
+print("Enter Deductions:")
+Deductions=int(input())
+
+total=Pay-Deductions
+
+print(Name)
+print("Total Pay:", total)
+
---
+++ main.py
@@ -0,0 +1,9 @@
+print("Enter the value of a:")
+a=int(input())
+
+print("Enter the value of b:")
+b=int(input())
+
+c = a + b
+
+print("The summation of a and b=:", c)
